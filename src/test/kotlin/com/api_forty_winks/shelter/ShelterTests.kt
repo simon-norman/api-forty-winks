423bee0589f7e046bce6631c1376396dc58d0b1f
Simon <01secn@gmail.com>
2019-04-17 18:11:39 +0100

    Refactored shelter tests - moved shared set up into before function - made tests more DRY

--- src/test/kotlin/com/api_forty_winks/shelter/ShelterTests.kt
+++ src/test/kotlin/com/api_forty_winks/shelter/ShelterTests.kt
@@ -6,45 +6,42 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.junit.Before
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class ShelterTests {
+    private var shelterParams = mutableMapOf<String, String>()
+    private lateinit var shelter :Shelter
+
+    @Before
+    fun testSetUp() {
+        shelterParams.put("name","Makers")
+        shelterParams.put("price","20.00")
+        shelterParams.put("description","40-room hostel for men and women")
+        shelterParams.put("location","Commercial Street, Aldgate")
+
+        shelter = Shelter(shelterParams)
+    }
+
     @Test
     fun `It returns a name when called`(){
-        val shelterParams = mutableMapOf<String,String>()
-        shelterParams.put("name","Makers")
-        val shelter = Shelter(shelterParams)
         assertEquals("Makers", shelter.name)
     }
 
     @Test
     fun `It returns a price when called`(){
-        val shelterParams = mutableMapOf<String,String>()
-        shelterParams.put("name","Makers")
-        shelterParams.put("price","20.00")
-        val shelter = Shelter(shelterParams)
         assertEquals("20.00", shelter.price)
     }
 
     @Test
     fun `It returns a description when called`(){
-        val shelterParams = mutableMapOf<String,String>()
-        shelterParams.put("name","Makers")
-        shelterParams.put("price","20.00")
-        shelterParams.put("description","40-room hostel for men and women")
-        val shelter = Shelter(shelterParams)
         assertEquals("40-room hostel for men and women", shelter.description)
     }
 
     @Test
     fun `It returns a location when called`(){
-        val shelterParams = mutableMapOf<String,String>()
-        shelterParams.put("name","Makers")
-        shelterParams.put("price","20.00")
-        shelterParams.put("description","40-room hostel for men and women")
-        shelterParams.put("location","Commercial Street, Aldgate")
-        val shelter = Shelter(shelterParams)
         assertEquals("Commercial Street, Aldgate", shelter.location)
     }
 }
