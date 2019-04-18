4961992600c4b4f0a0f74232c31ba96702603e35
Simon <01secn@gmail.com>
2019-04-18 17:25:05 +0100

    Voucher being created in db saves amount

--- src/main/kotlin/com/api_forty_winks/voucher/Voucher.kt
+++ src/main/kotlin/com/api_forty_winks/voucher/Voucher.kt
@@ -4,11 +4,8 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "voucher")
-class Voucher(amount: Float) {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(columnDefinition = "serial") var id: Long? = null
-
-    var code: String? = null
+class Voucher(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(columnDefinition = "serial") var id: Long? = null,
+    var code: String? = null,
     var amount: Float? = null
-
-}
+)
